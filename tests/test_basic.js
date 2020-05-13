48820bfe0b6e3ff0224237d2598bb876f1ab3b8d
Max Metral <max@pyralis.com>
2020-05-12 22:20:20 -0400

    Fix tests

--- tests/test_basic.js
+++ tests/test_basic.js
@@ -4,11 +4,12 @@ import gitShallow from '../src';
 
 tap.test('test_basic', async (t) => {
   t.ok(gitShallow, 'Should export a symbol');
-  gitShallow({
-    repositoryUrl: 'git@github.com:gas-buddy/git-shallow.git',
+  const sha = gitShallow({
+    repositoryUrl: 'https://github.com/gas-buddy/git-shallow.git',
     repositoryPath: '.github/workflows',
     workingDirectory: 'local-test',
   });
-  t.ok(fs.existsSync('local-test/.github/workflows/nodejs.yaml'), 'File should exist');
-  t.notOk(fs.existsSync('local-test/package.json'), 'Should not fetch files outside of path');
+  t.ok(sha, 'Should get a SHA as a result of the operation');
+  t.ok(fs.existsSync('./local-test/.github/workflows/nodejs.yml'), 'File should exist');
+  t.notOk(fs.existsSync('./local-test/src/index.js'), 'Should not fetch files outside of path');
 });
